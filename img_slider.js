20f7b9f783d225cec5973c1c10944c0be6043181
Marco M <65206518+marco-a-moreno@users.noreply.github.com>
2022-06-16 20:04:51 -0400

    initial commit

---
+++ img_slider.js
@@ -0,0 +1,16 @@
+(function(){
+    var imgLen = document.getElementById(imgGallery);
+    var images = imgLen.getElementsByTagName(img);
+    var counter = 1;
+
+    if(counter <= images.length){
+        setInterval(function(){
+            images[0].src = images[counter].src;
+            console.log(images[counter].src);
+            counter++;
+            if(counter === images.length){
+                counter = 1;
+            }
+        }, 4000);
+    }
+});
